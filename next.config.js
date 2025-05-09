283abdb0c740a7ba5c25546fbc848d2cf9d28890
Levente <lecsovi13@gmail.com>
2025-05-09 23:18:59 +0200

    Updating and deploying

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // for static export (required by Netlify)
-};
+ };
 
-module.exports = nextConfig;
+export default nextConfig;
